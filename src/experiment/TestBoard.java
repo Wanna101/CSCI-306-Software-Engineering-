9e858afc33bfc61b465db159b7f2073b6f1385a4
davidyoung@mines.edu <davidyoung@mines.edu>
2022-02-28 10:05:53 -0700

    created methods in TestBoard, namely the constructor, calcTargets, getTargets, and getCell (Driver: David Young, Navigator: Isabelle Neckel)

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,30 @@
 package experiment;
 
-public class TestBoard {
+import java.util.*;
 
+public class TestBoard {
+	private ArrayList<TestBoardCell> cells;
+	private Set<TestBoardCell> targets = new HashSet<TestBoardCell>();
+	
+	public TestBoard() {
+		// this ("ClueLayout.csv", "ClueSetup.txt");
+	}
+	
+	/*
+	public TestBoard(String boardLayout, String boardKey) {
+		this.cells = new ArrayList <TestBoardCell>();
+	}
+	*/
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		// TODO
+	}
+	
+	public Set<TestBoardCell> getTargets() {
+		return targets;
+	}
+	
+	public TestBoardCell getCell(int row, int col) {
+		return new TestBoardCell(row, col);
+	}
 }
