e897e14108610c8fad60fc874f0ee32697ba05f6
davidyoung@mines.edu <davidyoung@mines.edu>
2022-02-28 10:08:04 -0700

    created methods in TestBoardCell, namely the constructor, addAdjacency, getAdjList, isRoom, setIsRoom, getOccupied, setOccupied (Driver: David Young, Navigator: Isabelle Neckel)

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,39 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.*;
 
+public class TestBoardCell {
+	private int row, column;
+	private Set<TestBoardCell> adjList = new HashSet<TestBoardCell>();
+	private boolean room = false;
+	private boolean occupied = false;
+	
+	public TestBoardCell(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public void addAdjacency(TestBoardCell cell) {
+		cell = new TestBoardCell(row, column);
+	}
+	
+	public Set<TestBoardCell> getAdjList() {
+		return adjList;
+	}
+	
+	public boolean isRoom() {
+		return room;
+	}
+	
+	public void setIsRoom(boolean bool) {
+		this.room = bool;
+	}
+	
+	public boolean getOccupied() {
+		return occupied;
+	}
+	
+	public void setOccupied(boolean bool) {
+		this.occupied = bool;
+	}
 }
