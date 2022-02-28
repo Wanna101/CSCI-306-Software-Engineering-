1596b369b8b4ef84c2d13b05446dce7fe72f8088
davidyoung@mines.edu <davidyoung@mines.edu>
2022-02-28 10:14:04 -0700

    created methods in BoardTestsExp, methods test if adjacency list is correct size and has exactly the right elements, tests top left corner and bottom right corner, tests right edge and left edge, tests targets with several rolls and start locations, tests occupied cells (Driver: David Young, Navigator: Isabelle Neckel)

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,19 +1,81 @@
 package tests;
 
+import java.util.*;
+import experiment.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BoardTestsExp {
-
+	TestBoard board;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		board = new TestBoard();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAdjacency() {
+		// tests if adjacency list is correct size and has exactly the right elements 
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(0, 1)));
+		Assert.assertEquals(2, testList.size());
+	}
+	
+	
+	@Test
+	void testAdjCorners() {
+		// tests top left corner
+		// tests bottom right corner
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(0, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(3, 3)));
+		Assert.assertEquals(2, testList.size());
+	}
+	
+	@Test
+	void testAdjEdges() {
+		// tests right edge
+		// tests left edge
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1, 3)));
+		Assert.assertTrue(testList.contains(board.getCell(3, 0)));
+	}
+	
+	@Test
+	void testTargetsNormal() {
+		// tests targets with several rolls and start locations
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+	}
+	
+	@Test
+	void testTargetsMixed() {
+		// tests occupied cells
+		board.getCell(0, 2).setOccupied(true);
+		board.getCell(1, 2).setIsRoom(true);
+		TestBoardCell cell = board.getCell(0, 3);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(3, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
 	}
 
 }
