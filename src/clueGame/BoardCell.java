dccd29f6941837c212b885bd3c7a29f146f130f3
davidyoung@mines.edu <davidyoung@mines.edu>
2022-03-04 12:50:45 -0700

    added methods BoardCell, isDoorway, getDoorDirection, isLabel, isRoomCenter, getSecretPassage to BoardCell.java (Driver: David Young, Navigator: Isabelle Neckel)

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -12,8 +12,37 @@ public class BoardCell {
 	private char secretPassage;
 	private Set<BoardCell> adjList;
 	
+	public BoardCell() {
+		// TODO
+	}
+	
 	public void addAdj(BoardCell adj) {
 		// TODO
+	}
+
+	public boolean isDoorway() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public DoorDirection getDoorDirection() {
+		// TODO Auto-generated method stub
+		return DoorDirection.NONE;
+	}
+
+	public boolean isLabel() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isRoomCenter() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public char getSecretPassage() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
 
