bd93b67bf34bef6ddb0a6c2adcb31deff47a7fc3
davidyoung@mines.edu <davidyoung@mines.edu>
2022-03-07 08:25:40 -0700

    Board.java: setup initialize function to perform a try/catch statement for loadSetupConfig and loadLayoutConfig to catch BadConfigFormatExceptions and FileNotFoundExceptions (also made sure to print approprate messages in red to the console using System.err.println()), setup loadSetupConfig to throw BadConfigFormatException and FileNotFoundException and reads Setup.txt files to check for BadConfigFormatExceptions, setup loadLayoutConfig to throw BadConfigFormatException and FileNotFoundException and reads Layout.csv files to check for BadConfigExceptions (also it stores coordinates of cell and any other information such as door direction, setting label cell, center cell, etc.), created private functions characterIsValid and getRoomChars to assist loadLayoutConfig in obtaining more data for the cell, pasted calcAdj, calcTargets, findAllTargets from TestBoard.java and commented it out for C15A-2, corrected the getters at the end of the file to return right values (Driver: David Young, Navigator: David Young)

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,6 +1,7 @@
 package clueGame;
 
 import java.util.*;
+import java.io.*;
 
 public class Board {
 	
@@ -25,7 +26,16 @@ public class Board {
      * initialize the board (since we are using singleton pattern)
      */
     public void initialize() {
-    	// TODO
+    	try {
+    		loadSetupConfig();
+    		loadLayoutConfig();
+    	} catch(BadConfigFormatException e) {
+    		System.err.println(e.getMessage());
+    		// System.out.println(e.getMessage());
+    	} catch(FileNotFoundException e) {
+    		System.err.println("File not found: " + e.getMessage());
+    		// System.out.println("File not found: " + e.getMessage());
+    	}
     }
     
     public void setConfigFiles(String layoutConfigFile, String setupConfigFile) {
@@ -33,36 +43,243 @@ public class Board {
 		this.setupConfigFile = setupConfigFile;
 	}
     
-    public void loadSetupConfig() {
-    	// TODO
+    public void loadSetupConfig() throws BadConfigFormatException, FileNotFoundException {
+    	// create FileReader and Scanner to read the file
+    	FileReader setupConfig = new FileReader("data\\" + setupConfigFile);
+    	Scanner fileScanner = new Scanner(setupConfig);
+    	
+    	String nextLine;
+    	
+    	// while there is something still in the file, goto next line
+    	while(fileScanner.hasNext()) {
+    		nextLine = fileScanner.nextLine();
+    		
+    		// skip lines with //
+    		if (nextLine.contains("//")) {
+    			continue;
+    		}
+    		
+    		String[] values = nextLine.split(",");
+    		String type = values[0].replaceAll("\\s", "");
+    		if (!type.equals("Room") && !type.equals("Space")) {
+    			throw new BadConfigFormatException("Type is not equal to 'Room' or 'Space' (loadSetupConfig() in Board.java)");    		
+    		}
+    		
+    		// ltrim rtim
+    		String label = values[1].replaceAll("^\\s+", "").replaceAll("\\s+$", "");     		
+    		char character = values[2].replaceAll("\\s", "").charAt(0);
+    		Room r = new Room();
+    		r.setName(label);
+    		roomMap.put(character, r);
+    	}
     }
     
-    public void loadLayoutConfig() {
-    	// TODO
+    public void loadLayoutConfig() throws BadConfigFormatException, FileNotFoundException {
+    	// map <string, string> 
+    	// map["0,0"] = "X"
+    	Map<String, String> coordinates = new HashMap<String, String>();
+    	
+    	// create FileReader and Scanner to read the file
+    	FileReader layoutConfig = new FileReader("data\\" + layoutConfigFile);
+    	Scanner fileScanner = new Scanner(layoutConfig);
+    	
+    	String nextLine;
+    	int columnCount = 0;
+    	int lineNumber = 1;
+    	
+    	// while there is something still in the file, goto next line
+    	while(fileScanner.hasNext()) {
+    		nextLine = fileScanner.nextLine();    		
+    		String[] values = nextLine.split(",");
+
+			if (columnCount == 0) {
+    			columnCount = values.length;
+    		}
+    		else if (values.length != columnCount) {
+    			throw new BadConfigFormatException("Config has incorrect number of values at line: " + lineNumber + " (loadLayoutConfig() in Board.java)");
+    		}
+    		
+    		// throws exception if character value is not valid
+			int col = 0;
+    		for (String s : values) {    			
+    			// storing "row, col" as a string for the key to the map
+    			// row == lineNumber - 1
+    			// col == col    			 
+    			int row = lineNumber - 1;
+    			String key = String.valueOf(row) + "," + String.valueOf(col);
+    			coordinates.put(key, s);
+    			col++;
+    			
+    			if (s.length() == 1) {
+    				if (characterIsValid(s) == false) {
+    	    			throw new BadConfigFormatException("Single character '" + s + "' is not a valid room at line: " + lineNumber + " (loadLayoutConfig() in Board.java)");
+    	    		}    				
+    			}
+    			else if (s.length() == 2) {
+    				if (characterIsValid(s.substring(0, 1)) == false) {
+    	    			throw new BadConfigFormatException("Single character '" + s + "' is not a valid room at line: " + lineNumber + " (loadLayoutConfig() in Board.java)");
+    	    		}
+    				if (characterIsValid(s.substring(1, 2)) == false) {
+    					throw new BadConfigFormatException("Symbol character '" + s + "' is not a symbol at line: " + lineNumber + " (loadLayoutConfig() in Board.java)");
+    				}
+    			} else {
+    				throw new BadConfigFormatException("Room / symbol '" + s + "' needs to be 1 or 2 characters at line: " + lineNumber + " (loadLayoutConfig() in Board.java)");
+    			}
+    		}
+    		lineNumber++;
+    	}
+    	
+    	numRows = lineNumber - 1;
+    	numColumns = columnCount;
+		grid = new BoardCell[numRows][numColumns];
+		
+		for (int row = 0; row < numRows; row++) {
+			for (int col = 0; col < numColumns; col++) {
+				BoardCell currentCell = new BoardCell(row, col);
+				grid[row][col] = currentCell;
+				String key = String.valueOf(row) + "," + String.valueOf(col);
+				// get character(s) value of row, col
+				String roomAndSymbol = coordinates.get(key);			
+				String room = roomAndSymbol.substring(0, 1);
+				currentCell.setInitial(room.charAt(0));
+				
+				// we have a symbol
+				if (roomAndSymbol.length() == 2) {
+					String symbol = roomAndSymbol.substring(1, 2);
+					if (symbol.equals("<")) {
+						currentCell.setDoorDirection(DoorDirection.LEFT);
+					}
+					else if (symbol.equals(">")) {
+						currentCell.setDoorDirection(DoorDirection.RIGHT);
+					}
+					else if (symbol.equals("^")) {
+						currentCell.setDoorDirection(DoorDirection.UP);
+					}
+					else if (symbol.equals("v")) {
+						currentCell.setDoorDirection(DoorDirection.DOWN);
+					} 
+					else if (symbol.equals("*")) {
+						currentCell.setDoorDirection(DoorDirection.NONE);
+			    		roomMap.get(room.charAt(0)).setCenterCell(currentCell);
+			    		currentCell.setIsCenterCell();
+					}
+					else if (symbol.equals("#")) { 
+						currentCell.setDoorDirection(DoorDirection.NONE);
+			    		roomMap.get(room.charAt(0)).setLabelCell(currentCell);
+			    		currentCell.setIsLabelCell();
+					}
+					// secret passage letter
+					else {
+						currentCell.setDoorDirection(DoorDirection.NONE);
+						currentCell.setSecretPassage(symbol.charAt(0));						
+					}
+				}
+			}
+		}
+		
+		// calcAdj();
     }
+    
+    // After reading setup file, if you find a character on board not on setup file, throw
+    // >, <, ^, v, *, #, or simply a character, it is not the correct format and should be thrown
+    private boolean characterIsValid(String boardChar) {
+    	// check for valid character by putting all valid symbols and characters into a regex
+        // https://www.tutorialspoint.com/java-regular-expression-to-check-if-a-string-contains-alphabet
+		String validSymbols = "><v^*#";
+        String validLetters = this.getRoomChars();
+        String regex = "[" + validLetters + validSymbols + "]";
+        Pattern pattern = Pattern.compile(regex);
+        return boardChar.matches(regex);
+	}
+    
+    private String getRoomChars() {
+    	String chars = "";
+    	for (Map.Entry<Character, Room> entry : roomMap.entrySet()) {
+    		Character key = entry.getKey();
+    		chars += key.toString();
+    	}
+    	return chars;
+    }
+    
+    /*
+    // calculate adjacency list somewhere after the constructor
+ 	public void calcAdj() {
+ 		for (int row = 0; row < ROWS; row++) {			
+ 			for (int col = 0; col < COLS; col++) {
+ 				TestBoardCell currCell = grid[row][col];
+ 				// System.out.println("Current Cell (" + row + ", " + col + ")");
+ 				// check above
+ 				if ((row - 1) >= 0) {
+ 					currCell.addAdjacency(grid[row - 1][col]);
+ 				}	
+ 				// check below
+ 				if ((row + 1) < ROWS) {
+ 					currCell.addAdjacency(grid[row + 1][col]);
+ 				}
+ 				// check left
+ 				if ((col - 1) >= 0) {
+ 					currCell.addAdjacency(grid[row][col - 1]);
+ 				}
+ 				// check right
+ 				if ((col + 1) < COLS) {
+ 					currCell.addAdjacency(grid[row][col + 1]);
+ 				}
+ 			}
+ 		}
+ 	}
+ 	
+ 	public void calcTargets(TestBoardCell startCell, int pathlength) {
+ 		// clear previous memory
+ 		visited.clear();
+ 		targets.clear();
+ 		// add starting location to visited list 
+ 		visited.add(startCell);
+ 		// call recursive function findAllTargets
+ 		findAllTargets(startCell, pathlength);
+ 	}
+ 	
+ 	public void findAllTargets(TestBoardCell thisCell, int numSteps) {
+ 		// get adjacency list of current cell
+ 		// need to iterate through adjacency list
+ 		for (TestBoardCell adjCell : thisCell.getAdjList()) {
+ 			// needs to also test if the space is occupied by a person
+ 			if (visited.contains(adjCell) || adjCell.getOccupied()) {
+ 				continue;
+ 			}			
+ 			visited.add(adjCell);
+ 			// needs to also test if the space is a room therefore adding it as a potential target
+ 			if (numSteps == 1 || adjCell.isRoom()) {
+ 				targets.add(adjCell);
+ 			} else {
+ 				findAllTargets(adjCell, numSteps - 1);
+ 			}
+ 			visited.remove(adjCell);
+ 		}
+ 	}
+ 	
+ 	public Set<TestBoardCell> getTargets() {
+ 		return targets;
+ 	}
+    */
     
     public Room getRoom(char c) {
-		// TODO Auto-generated method stub
-		return new Room();
+		return roomMap.get(c);
+	}
+    
+    public Room getRoom(BoardCell cell) {
+		return getRoom(cell.getInitial());
 	}
     
     public int getNumRows() {
-		// TODO Auto-generated method stub
-		return 0;
+		return numRows;
 	}
 
-	public int getNumColumns() {
-		// TODO Auto-generated method stub
-		return 0;
+    public int getNumColumns() {
+		return numColumns;
 	}
 
-	public BoardCell getCell(int row, int col) {
-		// TODO Auto-generated method stub
-		return new BoardCell();
+    public BoardCell getCell(int row, int col) {
+		return grid[row][col];
 	}
 
-	public Room getRoom(BoardCell cell) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
