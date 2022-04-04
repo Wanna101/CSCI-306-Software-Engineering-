acc81bc24d687cb2a9211d1435c5213dc405a65a
davidyoung@mines.edu <davidyoung@mines.edu>
2022-04-04 09:10:12 -0600

    created the two test classes, ComputerAITest and GameSolutionTest (Driver: David Young, Navigator: Isabelle Neckel)

---
+++ src/tests/ComputerAITest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import clueGame.*;
+
+public class ComputerAITest {
+	private static Board board;
+	
+	@BeforeAll
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		// Initialize will load BOTH config files
+		board.initialize();
+	}
+}
---
+++ src/tests/GameSolutionTest.java
@@ -0,0 +1,61 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+
+import java.util.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import clueGame.*;
+
+public class GameSolutionTest {
+	private static Board board;
+	public static HumanPlayer human = new HumanPlayer();
+	public static ComputerPlayer pc = new ComputerPlayer();
+	public static Solution suggestion;
+	public static Solution suggestion2;
+	public static Solution suggestion3;
+	public static Solution suggestion4;
+	
+	@BeforeAll
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		// Initialize will load BOTH config files
+		board.initialize();
+		
+		board.getDeck().clear();
+		board.getPlayers().clear();
+		
+		Card a = new Card("Alderson", CardType.ROOM);
+		Card p = new Card("PCJ", CardType.PERSON);
+		Card o = new Card("Orecart", CardType.WEAPON);
+		Card h = new Card("Hill Hall", CardType.ROOM);
+		Card m = new Card("Mark Baldwin", CardType.PERSON);
+		Card c = new Card("Cardboard Boat", CardType.WEAPON);
+		
+		// board.overwriteSolution(new Solution(new Card("Hill Hall", CardType.ROOM), new Card("Mark Baldwin", CardType.PERSON), new Card("Cardboard Boat", CardType.WEAPON)));
+		board.overwriteSolution(new Solution(a, p, o));
+
+		suggestion = new Solution(a, p, o);
+		suggestion2 = new Solution(h, p, o);
+		suggestion3 = new Solution(a, m, o);
+		suggestion4 = new Solution(a, p, c);
+		
+		human.updateHand(new Card("Blaster", CardType.PERSON));
+		human.updateHand(new Card("CoorsTek", CardType.ROOM));
+		human.updateHand(new Card("Dynamite", CardType.WEAPON));
+		
+		pc.updateHand(new Card("Marvin", CardType.PERSON));
+		pc.updateHand(new Card("Marquez", CardType.ROOM));
+		pc.updateHand(new Card("Blaster Blaster", CardType.WEAPON));
+	}
+	
+	
+}
